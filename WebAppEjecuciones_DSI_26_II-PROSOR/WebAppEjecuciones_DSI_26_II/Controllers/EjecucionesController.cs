d317ac5cf91ffad3f22e8d3af8befb9048e36137
Thomas <beriagw12@gmail.com>
2026-09-07 08:00:42 -0500

    primer comentario

---
+++ WebAppEjecuciones_DSI_26_II-PROSOR/WebAppEjecuciones_DSI_26_II/Controllers/EjecucionesController.cs
@@ -0,0 +1,21 @@
+﻿using BackEndWebAppEjecuciones_DSI_26_II.Data.AccesoDatos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndWebAppEjecuciones_DSI_26_II.Controllers
+{
+    public class EjecucionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ObjDetalleEjecucion = new DADetalleEjecuciones();
+            var model=ObjDetalleEjecucion.GetDetalleOperacion();
+            return View(model);
+        }
+        public IActionResult VBEjecuciones()
+        {
+            var ObjVBEjecuciones = new DADetalleEjecuciones();
+            ViewBag.Ejecuciones = ObjVBEjecuciones.GetDetalleOperacion();
+            return View();
+        }
+    }
+}
---
+++ WebAppEjecuciones_DSI_26_II-PROSOR/WebAppEjecuciones_DSI_26_II/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAppEjecuciones_DSI_26_II.Models;
+
+namespace WebAppEjecuciones_DSI_26_II.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
