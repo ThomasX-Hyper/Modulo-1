c3f3d97ab777296493d81b322ea6320dcd6f7f63
Thomas <beriagw12@gmail.com>
2026-09-07 07:54:26 -0500

    Primer commit

---
+++ WebAppEjecuciones_DSI_26_II/Data/AccesoDatos/DADetalleEjecuciones.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppEjecuciones_DSI_26_II.Models;
+
+namespace WebAppEjecuciones_DSI_26_II.Data.AccesoDatos
+{
+    public class DADetalleEjecuciones
+    {
+
+        public IEnumerable<DetalleOperaciones> GetDetalleOperaciones()
+        {
+            var DetOperacion = new List<DetalleOperaciones>();
+            using(var db=new ApplicationDbContext())
+            {
+
+                DetOperacion=db.DetalleOperacion.Include(item=>item.CabeceraOperacion).ToList();
+            }
+
+            return DetOperacion;
+
+            }
+    }
+}
---
+++ WebAppEjecuciones_DSI_26_II/Models/DetalleOperaciones.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WebAppEjecuciones_DSI_26_II.Models
+{
+    public class DetalleOperaciones
+    {
+        [Key]
+        [Display(Name ="id")]
+        [Required]
+        public int IdDetalleOperacion { get; set; }
+        [Display(Name ="Descripcion")]
+        [Required(ErrorMessage ="Debe ingresar la descripcion del mensaje")]
+        [MaxLength(100,ErrorMessage ="El campo no debe tener mas de 100 caracteres")]
+
+        public string Descripcion { get; set; }
+        [Display(Name ="Cantidad")]
+        [Required]
+        public int Cantidad { get; set; }
+        [Display(Name ="precio")]
+        [Required(ErrorMessage ="Debe ingresar el preciode la operacion")]
+
+        public float Importe { get; set; }
+
+        [Display(Name ="igv")]
+        [Required]
+
+        public float Igv { get; set; }
+        [Display (Name ="total")]
+        [Required]
+
+        public float total { get; set; }
+
+        public int IdCabeceraOperacion { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey("IdCabeceraOperacion")]
+        public virtual CabeceraOperacion CabeceraOperacion { get; private set; }
+    }
+}
